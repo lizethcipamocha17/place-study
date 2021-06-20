caed56c9f39354340576694b1543a8f544c98489
Lizeth Cipamocha <lizeth.cipamocha@usantoto.edu.co>
2021-06-20 13:32:13 -0500

    variables de entorno

--- webchatbot/settings/production.py
+++ webchatbot/settings/production.py
@@ -1,13 +1,13 @@
 
 from .base import *
 
-# DEBUG = config('DEBUG')
-DEBUG = True
+DEBUG = config('DEBUG')
+# DEBUG = True
 ALLOWED_HOSTS = ['https://djangoapichatbot.herokuapp.com/']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST': config('DB_HOST'),  # env
         'NAME': config('DB_NAME'),  # env
         'USER': config('DB_USER'),  # env
