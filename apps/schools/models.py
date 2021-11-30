c15af58c9e449115db2db79077336e3fb3044d1a
Lizeth Cipamocha <lizeth.cipamocha@usantoto.edu.co>
2021-11-30 10:16:26 -0500

    agregar campo en el modelo school y actualizar las migraciones

--- apps/schools/models.py
+++ apps/schools/models.py
@@ -2,9 +2,12 @@ from django.core.validators import FileExtensionValidator
 from django.db import models
 
 
-
 def upload_to(instance, filename):
     return 'content/{filename}'.format(filename=filename)
+
+
+def upload_to_school(instance, filename):
+    return 'school/{filename}'.format(filename=filename)
 
 
 # Create your models here.
@@ -12,6 +15,9 @@ class School(models.Model):
     school_id = models.BigAutoField(primary_key=True)
     location = models.ForeignKey('accounts.Location', on_delete=models.CASCADE, verbose_name='ubicación')
     school_name = models.CharField('nombre del colegio', max_length=120)
+    photo = models.ImageField('Logo o escudo de un colegio', upload_to=upload_to_school,
+                              validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])]
+                              )
 
     class Meta:
         db_table = 'school'
